105228f3e62c3ce50433cbf2920734679657121d
Unknown <gczernatowicz1@gmail.com>
2019-05-21 21:38:47 +0200

    working interrupt from mock switch

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #define ESP_INTR_FLAG_DEFAULT 0
 
 #define BLINK_GPIO CONFIG_BLINK_GPIO
-#define REED_IO_NUM 13
+#define REED_IO_NUM 18
 
 void blink_task(void *pvParameter)
 {
@@ -31,9 +31,11 @@ void blink_task(void *pvParameter)
         gpio_set_level(BLINK_GPIO, 1);
     }
 }
-
+//IRAM_ATTR - function with this will be moved to RAM in order to execute faster than default from flash
+volatile int count = 0;
 static void IRAM_ATTR io_intr_handler(void* arg) {
-    printf("Interrupt");
+    count++;
+    ets_printf("intr %d\n",count);
 }
 
 void app_main()
@@ -45,12 +47,11 @@ void app_main()
     io_conf.mode = GPIO_MODE_INPUT;
     //bit mask of the pins that you want to set,e.g.GPIO18/19
     io_conf.pin_bit_mask = 1ULL<<REED_IO_NUM;
-    io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 1;
-    io_conf.intr_type = GPIO_PIN_INTR_NEGEDGE;
+    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
     //configure GPIO with the given settings
     gpio_config(&io_conf);
-
+    gpio_set_intr_type(REED_IO_NUM, GPIO_INTR_POSEDGE);
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     gpio_isr_handler_add(REED_IO_NUM, io_intr_handler, (void*) REED_IO_NUM);
     
