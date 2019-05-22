ab3240cdb9e393e452a052758968830a7316870e
Unknown <gczernatowicz1@gmail.com>
2019-05-22 14:46:39 +0200

    use queue for reed interrupt

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
@@ -9,17 +10,16 @@
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 #define REED_IO_NUM 18
 
+static xQueueHandle reed_evt_queue = NULL;
+
 void blink_task(void *pvParameter)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
     portTickType xLastWakeTime;
     printf("[Blinker] Starting\n");
     printf("[Blinker] Configuring GPIOS\n");
+    /* specify that the function of a given pin 
+    should be that of GPIO as opposed to some other function 
+    */
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
@@ -31,17 +31,42 @@ void blink_task(void *pvParameter)
         gpio_set_level(BLINK_GPIO, 1);
     }
 }
+
+static void check_status(void *arg) {
+    int msg_count;
+    while(true) {
+        msg_count = uxQueueMessagesWaitingFromISR(reed_evt_queue);
+        printf("[STATUS]queue: %d\n", msg_count);
+        vTaskDelay(1000/portTICK_RATE_MS);
+    }
+}
+
+static void reed_task(void* arg)
+{
+    uint32_t counter = 0;
+    int val;
+    for(;;) {
+        if(xQueueReceive(reed_evt_queue, &val, portMAX_DELAY)) {
+            counter++;
+            printf("[REED] %d!\n",counter);
+        }
+    }
+}
+
 //IRAM_ATTR - function with this will be moved to RAM in order to execute faster than default from flash
-volatile int count = 0;
 static void IRAM_ATTR io_intr_handler(void* arg) {
-    count++;
-    ets_printf("intr %d\n",count);
+    // send GPIO for now, consider sending lastWakeTime?
+     uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(reed_evt_queue, &gpio_num, NULL);
 }
 
 void app_main()
 {
     printf("IDF version: %s\n",esp_get_idf_version());
     printf("Starting OBC. Tasks running: %d\n",uxTaskGetNumberOfTasks());
+    printf("Init reed queue\n");
+    reed_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
     printf("Attaching reed switch interrupt\n");
     gpio_config_t io_conf;
     io_conf.mode = GPIO_MODE_INPUT;
@@ -54,6 +79,12 @@ void app_main()
     gpio_set_intr_type(REED_IO_NUM, GPIO_INTR_POSEDGE);
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     gpio_isr_handler_add(REED_IO_NUM, io_intr_handler, (void*) REED_IO_NUM);
-    
+
+    printf("Initialize blinking!\n");
     xTaskCreate(&blink_task, "blink_task", 2048, NULL, 5, NULL);
+    printf("Start reed switch task!\n");
+    xTaskCreate(&reed_task, "reed_task", 2048, NULL, 1, NULL);
+    printf("Start status checker task\n");
+    xTaskCreate(&check_status, "check_status_task", 2048, NULL, 5, NULL);
+    printf("OBC startup complete. Tasks running: %d\n",uxTaskGetNumberOfTasks());
 }
