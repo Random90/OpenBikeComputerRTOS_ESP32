a4a0fea5d12a2cf30eecc5c32c9b46da31add81e
Random90 <gczernatowicz1@gmail.com>
2020-03-24 22:26:44 +0100

    glcd byte arr converter v2 it works, but char is rotated

--- utils/mirrorGLCDChar.js
+++ utils/mirrorGLCDChar.js
@@ -1,25 +1,72 @@
-string = "01100000,00011000,00111000,01000000,11100000,00000001,11100000,00000001,11100000,00000001,11111110,00000001,11111110,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11111110,00011111,11111110,00011111,00000000,00000000,00000000,00000000";
+string = "00000000,00000000,00000000,00000000,11100000,00000001,11100000,00000001,11100000,00000001,11111110,00000001,11111110,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11100000,00000001,11111110,00011111,11111110,00011111,00000000,00000000,00000000,00000000";
 
-const LINES = 2;
+const COLUMNS = 2;
 
-let string2 = '';
-let buffer = '';
-let linesCounter = 0;
-for(let i = 0; i < string.length; i++) {
-  buffer += string[i];
-  if (string[i] === ',' || i === string.length - 1) {
-    linesCounter++;
-    string2 += 
-      mirrorByte(buffer) + 
-      (i === string.length - 1  ? '' : ',');
+let resultStr = '';
+let buffer = [['']];
+let columnsCounter = 0;
+let row = 0;
 
-    if (linesCounter === LINES) {
-      linesCounter = 0;
-      string2 += "\n";
+fillTheBuffer();
+mirrorAllBytes();
+prependBinarySign();
+printColumnsLastRowsFirst();
+//printRowsFirstColumnsLast();
+
+function fillTheBuffer() {
+  for(let i = 0; i < string.length; i++) {
+    if (string[i] === ',') {
+      if (columnsCounter === COLUMNS - 1) {
+        columnsCounter = 0;
+        row++;
+        buffer[row] = [[]];
+      } else {
+        columnsCounter++;
+        buffer[row][columnsCounter] = '';
+      }
+    } else {
+      buffer[row][columnsCounter] += string[i];
     }
-    buffer = '';
-    
   }
+}
+
+function mirrorAllBytes() {
+  [...Array(row)].forEach((_, i) => {
+    [...Array(COLUMNS)].forEach((_, j) => {
+      buffer[i][j] = mirrorByte(buffer[i][j]);
+    });
+  });
+}
+
+function prependBinarySign() {
+  [...Array(row + 1)].forEach((_, i) => {
+    [...Array(COLUMNS)].forEach((_, j) => {
+      buffer[i][j] = '0b' + buffer[i][j];
+    });
+  });
+}
+
+function printColumnsLastRowsFirst() {
+  
+    for (let j = COLUMNS - 1; j >= 0; j--) {
+      [...Array(row)].forEach((_, i) => {
+        resultStr += buffer[i][j]  + (i === row - 1 ? '' : ',');
+        resultStr += '\n';
+      });
+      resultStr += '\n';
+    }
+    
+}
+
+function printRowsFirstColumnsLast() {
+  
+  [...Array(row + 1)].forEach((_, i) => {
+    [...Array(COLUMNS)].forEach((_, j) => {
+      resultStr += buffer[i][j]  + (i === row && j === COLUMNS - 1 ? '' : ',');
+      resultStr += '\n';
+    });
+  });
+  
 }
 
 function mirrorByte(byteStr) {
@@ -29,4 +76,5 @@ function mirrorByte(byteStr) {
   }
   return buffer;
 }
-console.log(string2);
+console.log(resultStr);
+return resultStr;
