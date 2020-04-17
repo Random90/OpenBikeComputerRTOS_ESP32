489b5c95f198217340e6298dc48eff035dea81b3
Random90 <gczernatowicz1@gmail.com>
2020-04-17 23:31:21 +0200

    rendering speed finished!

--- main/Tasks/screen_pcd8544/pcd8544_font_utils.c
+++ main/Tasks/screen_pcd8544/pcd8544_font_utils.c
@@ -1,35 +1,34 @@
 #include <pcd8544_font_utils.h>
-#include "esp_log.h"
+#include <string.h>
 
+#include "esp_log.h"
 #define TAG "PCD8544"
 
 uint8_t **getSpeedChars(float *speed) {
     // lifetime array of pointers to chars
     static uint8_t *char_arr[4];
     // create buffer for converted float
-    char buffer1[5];
-    char buffer2[6];
-    // convert float to string
-    int size = snprintf(buffer1, 4,"%0.2f", *speed);
+    char buffer[10];
+    // convert float to int dot int
+    uint8_t speedInt = *speed;
+    float tempFrac = *speed - speedInt;
+    uint8_t fraction = (tempFrac * 10);
     // pad str with 0 if speed lower than 10
-    // FIXME optimize
-    if (size < sizeof(buffer1)) {
-        sprintf(buffer2, "0%s", buffer1);
+    if(speedInt < 10) {
+        snprintf(buffer, 10, "0%d.%d", speedInt, fraction);  
     } else {
-        sprintf(buffer2, "%s", buffer1);
+        snprintf(buffer, 10, "%d.%d", speedInt, fraction);
     }
-    ESP_LOGI(TAG, "%s", buffer1);
+    ESP_LOGI(TAG, "hmmm %s", buffer);
     for (int i = 0; i <= 4; i++) {
         // convert to int and fill char_arr with pointers to big font characters
-        if (buffer2[i] != '.') {
-            char single_char_buf[1] = {buffer2[i]};
+        if (buffer[i] != '.') {
+            char single_char_buf[1] = {buffer[i]};
             char_arr[i] = fontDetermination[atoi(single_char_buf)];
         } else {
-            // TODO add dot? drewinf rectangle is buggy
+            // TODO add dot? drawing rectangle is buggy ofc
             continue;
         }
-        
-        
     }
     return &char_arr;
 }
