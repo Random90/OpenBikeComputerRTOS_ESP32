52b73cabe3430896ba7457f349c94fc4d1c12c17
Random90 <gczernatowicz1@gmail.com>
2020-05-10 21:56:18 +0200

    fixed big diffMS after sleep - fixed bad averages and times after waking up from deep sleep by skipping first interrupt

--- main/main.c
+++ main/main.c
@@ -75,6 +75,7 @@ static void vRideStatusIntervalCheckTask(void *arg) {
             rideParams.speed = 0.0;
             rideParams.msBetweenRotationTicks = 0;
             rideParams.moving = false;
+            rideParams.prevRotationTickCount = 0;
             // TODO add semaphore and run data saving, stop screen refresh, run powersave algorithms, sync with could etc
         } 
         vTaskDelayUntil(&currentTickCount, 1000/portTICK_RATE_MS);
@@ -89,14 +90,15 @@ static void vCalcRideParamsOnISRTask(void* data)
         if(xQueueReceive(reed_evt_queue, &rideParams.rotationTickCount, portMAX_DELAY)) {
             // TODO create buffer for reed time impulses before calculating time and speed
             // TODO block rideParams while calculating?
-            
-            rideParams.moving = true;
-            rideParams.rotations++;
-            rideParams.msBetweenRotationTicks = ((int) rideParams.rotationTickCount - (int) rideParams.prevRotationTickCount) * (int) portTICK_RATE_MS;
-            rideParams.totalRideTimeMs += rideParams.msBetweenRotationTicks;
-            rideParams.speed = ( (float) CIRCUMFERENCE/1000000 ) / ( (float) rideParams.msBetweenRotationTicks / 3600000 ); //km/h
-            rideParams.distance = (float)rideParams.rotations * (float)CIRCUMFERENCE/1000000;
-            rideParams.avgSpeed = rideParams.distance / ( (float) rideParams.totalRideTimeMs / 3600000 );
+            if (rideParams.prevRotationTickCount != 0) {
+                rideParams.moving = true;
+                rideParams.rotations++;
+                rideParams.msBetweenRotationTicks = ((int) rideParams.rotationTickCount - (int) rideParams.prevRotationTickCount) * (int) portTICK_RATE_MS;
+                rideParams.totalRideTimeMs += rideParams.msBetweenRotationTicks;
+                rideParams.speed = ( (float) CIRCUMFERENCE/1000000 ) / ( (float) rideParams.msBetweenRotationTicks / 3600000 ); //km/h
+                rideParams.distance = (float)rideParams.rotations * (float)CIRCUMFERENCE/1000000;
+                rideParams.avgSpeed = rideParams.distance / ( (float) rideParams.totalRideTimeMs / 3600000 );
+            }
             rideParams.prevRotationTickCount = rideParams.rotationTickCount;    
 
             ESP_LOGI(TAG, "[REED] count: %d, speed: %0.2f, diff: %d, distance: %0.2f", rideParams.rotations, rideParams.speed, rideParams.msBetweenRotationTicks, rideParams.distance);
