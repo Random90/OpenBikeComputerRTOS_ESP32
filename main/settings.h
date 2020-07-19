220410c528e5f8ce9e09f95d69566d46f543ad96
Random90 <gczernatowicz1@gmail.com>
2020-07-19 19:46:55 +0200

    Added KConfig menus - moved circumference to KConfig

--- main/settings.h
+++ main/settings.h
@@ -2,7 +2,7 @@
 #define SETTINGS
 
 // tire circuference in milimeters
-#define CIRCUMFERENCE 2012
+#define CIRCUMFERENCE CONFIG_WHEEL_CIRCUMFERENCE
 /* Time after which OBC enters 'not riding' mode*/
 #define RIDE_TIMEOUT_MS 2500
 
