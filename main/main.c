1257d227c14da4667184b9715273e1f9b114110c
Random90 <gczernatowicz1@gmail.com>
2020-07-19 22:13:59 +0200

    Test GET request to OBC Server

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -11,8 +12,13 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+
 #include "lwip/err.h"
 #include "lwip/sys.h"
+
+#include "esp_http_client.h"
+#include "esp_tls.h"
+#include <string.h>
 
 #include "settings.h"
 #include "obc.h"
@@ -27,10 +33,27 @@
 
 #define TAG "OBC_MAIN"
 
-// @TODO move wifi functions somewhere else
+// @TODO move wifi/sync functions somewhere else
 #define WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define WIFI_PASSWD      CONFIG_ESP_WIFI_PASSWORD
 #define WIFI_MAX_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+#define WEB_SERVER CONFIG_OBC_SERVER_ADDR
+#define WEB_PORT CONFIG_OBC_SERVER_PORT
+
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static int s_retry_num = 0;
+
 
 // declare global queues 
 xQueueHandle reed_evt_queue = NULL;
@@ -52,21 +75,92 @@ ride_params_t rideParams = {
 TaskHandle_t screenRefreshTaskHandle = NULL;
 TaskHandle_t spiffsSyncOnStopTaskHandle = NULL;
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
-
-/* The event group allows multiple bits for each event, but we only care about two events:
- * - we are connected to the AP with an IP
- * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
-
-static int s_retry_num = 0;
-
 //IRAM_ATTR - function with this will be moved to RAM in order to execute faster than default from flash
 static void IRAM_ATTR vReedISR(void* arg) {
     portTickType xLastReedTickCount = xTaskGetTickCount();
     xQueueSendFromISR(reed_evt_queue, &xLastReedTickCount, NULL);
+}
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                if (output_buffer != NULL) {
+                    free(output_buffer);
+                    output_buffer = NULL;
+                }
+                output_len = 0;
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            break;
+    }
+    return ESP_OK;
+}
+
+static void http_test_task(void *pvParameters)
+{
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+
+    esp_http_client_config_t config = {
+        .host = WEB_SERVER,
+        .port = WEB_PORT,
+        .path = "/",
+        .query = "",
+        .event_handler = _http_event_handler,
+        .user_data = local_response_buffer,        // Pass address of local buffer to get response
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET
+    esp_http_client_set_method(client, HTTP_METHOD_GET);
+    esp_http_client_set_header(client, "X-Requested-With", "OBC");
+    esp_http_client_set_header(client, "accept", "application/json");
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+
+    esp_http_client_cleanup(client);
+
+    ESP_LOGI(TAG, "Finish http example");
+    vTaskDelete(NULL);
 }
 
 void vInitTasks() {
@@ -75,6 +169,7 @@ void vInitTasks() {
     xTaskCreate(&vRideStatusWatchdogTask, "vRideStatusIntervalCheckTask", 2048, NULL, 3, NULL);
     xTaskCreate(&vSpiffsSyncOnStopTask, "vSpiffsSyncOnStopTask", 2048, NULL, 3, &spiffsSyncOnStopTaskHandle);
     xTaskCreate(&vScreenRefreshTask, "vScreenRefreshTask", 2048, NULL, 2, &screenRefreshTaskHandle);
+    xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL);
 }
 
 void vAttachInterrupts() {
@@ -116,6 +211,7 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
     }
 }
 
+//@TODO move to task and block http tasks with semaphores or by events
 void vWifiInitStation(void)
 {
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA INIT");
@@ -162,6 +258,7 @@ void vWifiInitStation(void)
     } else {
         ESP_LOGE(TAG, "UNEXPECTED WIFI ERROR");
     }
+    
 
     ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler));
     ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler));
