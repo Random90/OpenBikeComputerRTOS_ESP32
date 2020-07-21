f3a3e63d6e0251f99f9849f495cd0d957801b2ff
Random90 <gczernatowicz1@gmail.com>
2020-07-21 20:17:38 +0200

    updated inlclude guards

--- main/Tasks/sync/obc_rest.task.h
+++ main/Tasks/sync/obc_rest.task.h
@@ -1,4 +1,9 @@
+#ifndef TASKS_SYNC_OBC_REST_H
+#define TASKS_SYNC_OBC_REST_H
+
 /**
  * @brief task used for data online synchronization
  * */
 void vHttpSyncRest(void *pvParameters);
+
+#endif
--- main/wifi.h
+++ main/wifi.h
@@ -1,4 +1,10 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+
 /**
  * @brief synchronously initialize wifi as a station and connect to sdkconfig specified wifi network
  * */
 void vInitWifiStation(void);
+
+#endif
